a1c780cf14a0f89339dd189c9947742e88778a19
Wimpy <25759224+aingwnf@users.noreply.github.com>
2019-12-11 16:39:24 +0700

    Delete Low Stock Backup - Old
    Delete Low Stock Backup - Old

--- addons/bi_product_low_stock_notification_backup/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
-import inherited_res_company
-
-import inherited_stock_config_settings
-
-import inherited_product_product
-
